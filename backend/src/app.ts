5dbffd079a401acd55eca91ffa5293eeefbf9c84
christiansaim77-hash <christiansaim77@gmail.com>
2026-07-30 05:54:08 +0800

    Create backend/src/app.ts
    // Routes (to be registered as modules are created)

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import { authRoutes } from './modules/auth/routes';
+import { userRoutes } from './modules/users/routes';
+import { postRoutes } from './modules/posts/routes';
+import { prayerRoutes } from './modules/prayer/routes';
+import { bibleRoutes } from './modules/bible/routes';
+import { groupRoutes } from './modules/groups/routes';
+import { messagingRoutes } from './modules/messaging/routes';app.get('/api/v1/modules', (req, res) => {
+  res.json({
+    modules: [
+      'auth',
+      'users',
+      'posts',
+      'prayer',
+      'bible',
+      'groups',
+      'messaging'
+    ],
+    status: 'loaded'
+  });
+});
